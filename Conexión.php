31df25d9af73dd51d7b4f6af83ca346993ab70ce
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-08-24 23:15:11 -0500

    Base de datos

--- Conexión.php
+++ Conexión.php
@@ -1,13 +1,12 @@
 <?php
     $servername = "localhost";
-    $database = $dbname = "usuarios";
+    $database = $dbname = "lista_equipos";
     $username = "dantecc10";
     $password = "@dantecc10!";
     // Create connection
-    $conn = mysqli_connect($servername, $username, $password, $database);
+    $conexión = mysqli_connect($servername, $username, $password, $database);
     // Check connection
-    if (!$conn) {
+    if (!$conexión) {
         die("<p>Error al conectar con la base de datos: " . mysqli_connect_error() . "</p><p>Pruebe a verificar la conexión del servidor...</p>");
     }
     echo "Conexión a la base de datos: Exitosa";
-?>
