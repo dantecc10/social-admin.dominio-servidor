b6d27476f66ace22d619c37a6f69a0d35d794ced
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-08-24 22:58:59 -0500

    Cargas iniciales

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <link rel="shortcut icon" href="../IMG/FAVICON TRANSPARENTE.png" type="image/x-icon">
+    <link id="EtiquetaCSS" rel="Stylesheet" href="../CSS/CSS GLOBAL - TEMA OSCURO.css" Type="text/css" MEDIA="screen">
+    <link id="Oculta/Muestra" rel="Stylesheet" href="../CSS/CSS OCULTAMIENTO.css" Type="text/css" MEDIA="screen">
+
+    <meta name="description" content="Dante Castelán Carpinteyro: Web dedicada a publicación de proyectos y código personal.">
+    <meta name="author" content="Dante Castelán Carpinteyro">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dante - Inicio</title>
+</head>
+
+<body>
+    <script src="../JavaScript/CambiarTema.js"></script>
+    <!--Encabezado (no mover)--> <br>
+    <header id="Encabezado">
+        <div id="Contenedor-Encabezado">
+            <div id="Contenido-Encabezado">
+                <h1>Inicio</h1> <a href="index.php"> <img id="Logo" src="../IMG/FAVICON.PNG" alt="dante.castelancarpinteyro.club">
+                </a>
+                <div class="Menú-Desplegable"> <button class="Botón-Desplegar">Menú</button>
+                    <div class="Contenido-Desplegable">
+                        <a href="IniciarSesión.php">Iniciar Sesión</a>
+                        <a href="politiza.castelancarpinteyro.club">POLITIZA MX</a>
+                        <a href="AcercaDe.php">Acerca de...</a>
+                        <a id="CambiaTema" onclick="javascript:CambiarTema();">Cambiar tema</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="Encabezado-Superior"></div>
+        <div id="Encabezado-Inferior"></div>
+    </header>
+
+    <h2 id="IndicaArtículos">Mi página de inicio</h2>
+    <div class="Contenedor-Artículos" id="PartidosPolíticos">
+        <h4 class="Autor-Artículo">Por Dante Castelán Carpinteyro.</h4>
+        <h3 class="TítuloArtículo">En construcción...</h3>
+        <p class="Descripción-Artículo">Pronto actualizaré el código de index... paciencia</p>
+    </div>
+</body>
+
+</html>
