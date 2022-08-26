63d8823791d71ffdbe697b04f6f6c9cfd71adc61
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-08-26 11:10:48 -0500

    Filtro ID AJAX

--- FiltrarID.php
+++ FiltrarID.php
@@ -1,5 +1,5 @@
 <?php
-echo "<script>console.log('Saludos del servidor');</script>";
+
 $ID = strval($_GET['ID']);
 //  $ModoFiltro = ($_GET['ModoFiltro']);
 $sql;
@@ -8,51 +8,61 @@ $sql;
   */
 
 echo ("ID es: " . $ID . "<br>");
-$SQLBase = "SELECT * FROM juguete WHERE ";
+$SQLBase = "SELECT * FROM `equipos_administrativos` WHERE ";
 $SQLBase = ($SQLBase . " `ID` = '" . $ID . "'");
 $sql = $SQLBase;
 ConstruirTabla($sql);
 
 function ConstruirTabla($sql)
 {
-    $con = mysqli_connect('localhost', 'root', '', 'life');
+    $con = mysqli_connect('localhost', 'dantecc10', '@dantecc10!', 'lista_equipos');
     if (!$con) {
         die('Could not connect: ' . mysqli_connect_error($con));
     }
     echo $sql;
     $result = mysqli_query($con, $sql) or die("Error en la consulta a la base de datos");
-    echo "<table class='Catálogo'>";
-    echo "<tr class='Fila1'>";
-    echo "<th class='CeldaParaID'>ID</th>";
-    echo "<th class='CeldaParaArtículo'>Artículo</th>";
-    echo "<th class='CeldaParaMarca'>Marca</th>";
-    echo "<th class='CeldaParaCantidad'>Cantidad</th>";
-    echo "<th class='CeldaParaPrecioL'>Precio local</th>";
-    echo "<th class='CeldaParaPrecioML'>Precio ML</th>";
-    echo "<th class='CeldaParaPrecioMS'>Precio MS</th>";
-    echo "<th class='CeldaParaLíneaSerie'>Línea / Serie</th>";
-    echo "<th class='CeldaParaModelo'>Modelo</th>";
-    echo "<th class='CeldaParaDescripción'>Descripción</th>";
-    echo "<th class='CeldaParaBarras'>Barras</th>";
-    echo "<th class='CeldaParaSKU'>SKU</th>";
+    echo "<tr>";
+    echo "<th>ID</th>";
+    echo "<th>Usuario</th>";
+    echo "<th>Nombre</th>";
+    echo "<th>Puesto</th>";
+    echo "<th>Dependencia</th>";
+    echo "<th>IP</th>";
+    echo "<th>Servidor</th>";
+    echo "<th>Inventario</th>";
+    echo "<th>Procesador</th>";
+    echo "<th>Frecuencia</th>";
+    echo "<th>RAM</th>";
+    echo "<th>Sistema Operativo</th>";
+    echo "<th>Marca</th>";
+    echo "<th>Modelo</th>";
+    echo "<th>ECOSYS 3145</th>";
+    echo "<th>TASKalfa</th>";
+    echo "<th>M5521</th>";
+    echo "<th>Estado</th>";
     echo "</tr>";
     while ($columna = mysqli_fetch_array($result)) {
-        echo "<tr class='FilasProductos'>";
-        echo "<td class = 'CeldaParaID'>" . $columna['ID'] . "</td>";
-        echo "<td class = 'CeldaParaArtículo'>" . $columna['Artículo'] . "</td>";
-        echo "<td class = 'CeldaParaMarca'>" . $columna['Marca'] . "</td>";
-        echo "<td class = 'CeldaParaCantidad'>" . $columna['Cantidad'] . "</td>";
-        echo "<td class = 'CeldaParaPrecioL'>$" . $columna['Precio L'] . "</td>";
-        echo "<td class = 'CeldaParaPrecioML'>$" . $columna['Precio ML'] . "</td>";
-        echo "<td class = 'CeldaParaPrecioMS'>$" . $columna['Precio MS'] . "</td>";
-        echo "<td class = 'CeldaParaLíneaSerie'>" . $columna['Línea / Serie'] . "</td>";
-        echo "<td class = 'CeldaParaModelo'>" . $columna['Modelo'] . "</td>";
-        echo "<td class = 'CeldaParaDescripción'>" . $columna['Descripción'] . "</td>";
-        echo "<td class = 'CeldaParaBarras'>" . $columna['Barras'] . "</td>";
-        echo "<td class = 'CeldaParaSKU'>" . $columna['SKU'] . "</td>";
+        echo "<tr>";
+        echo "<td>" . $columna['ID'] . "</td>";
+        echo "<td>" . $columna['Usuario'] . "</td>";
+        echo "<td>" . $columna['Nombre'] . "</td>";
+        echo "<td>" . $columna['Puesto'] . "</td>";
+        echo "<td>" . $columna['Dependencia'] . "</td>";
+        echo "<td>" . $columna['IP'] . "</td>";
+        echo "<td>" . $columna['Servidor'] . "</td>";
+        echo "<td>" . $columna['Inventario'] . "</td>";
+        echo "<td>" . $columna['Procesador'] . "</td>";
+        echo "<td>" . $columna['Frecuencia'] . "</td>";
+        echo "<td>" . $columna['RAM'] . "</td>";
+        echo "<td>" . $columna['Sistema Operativo'] . "</td>";
+        echo "<td>" . $columna['Marca'] . "</td>";
+        echo "<td>" . $columna['Modelo'] . "</td>";
+        echo "<td>" . $columna['ECOSYS 3145'] . "</td>";
+        echo "<td>" . $columna['TASKalfa'] . "</td>";
+        echo "<td>" . $columna['M5521'] . "</td>";
+        echo "<td>" . $columna['Estado'] . "</td>";
         echo "</tr>";
     }
     echo "</table>";
     mysqli_close($con);
 };
-?>
