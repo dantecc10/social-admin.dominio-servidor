8ae5ec5b77cd6c4b51426a81e631fe48561b9ffd
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-08-29 15:00:24 -0500

    Sin ocultar

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 
 <head>
     <link rel="shortcut icon" href="../IMG/FAVICON TRANSPARENTE.png" type="image/x-icon">
-    <link id="EtiquetaCSS" rel="Stylesheet" href="CSS/CSS GLOBAL - TEMA INSTITUCIONAL PUEBLA.css" Type="text/css" MEDIA="screen">
-    <link id="Oculta/Muestra" rel="Stylesheet" href="../CSS/CSS OCULTAMIENTO.css" Type="text/css" MEDIA="screen">
+    <link id="EtiquetaCSS" rel="Stylesheet" href="CSS/CSS GLOBAL - TEMA INSTITUCIONAL PUEBLA.css" type="text/css" media="screen">
+
     <link rel="stylesheet" href="CSS/Formulario.css" type="text/CSS">
     <meta name="description" content="Dante Castelán Carpinteyro: Web dedicada a publicación de proyectos y código personal.">
     <meta name="author" content="Dante Castelán Carpinteyro">
@@ -41,16 +41,16 @@
     <div id="DivInicioSesión" class="modal">
         <form class="modal-content animate" action="/action_page.php" method="post">
             <div class="imgcontainer">
-                <span onclick="document.getElementById('DivInicioSesión').style.display='none'" class="CerrarCuadro" title="Close Modal">&times;</span>
+                <span onclick="document.getElementById('DivInicioSesión').style.display='none'" class="CerrarCuadro" title="Close Modal">×</span>
                 <img src="IMG/img_avatar2.png" alt="Avatar" class="avatar">
             </div>
 
             <div class="container">
                 <label for="email"><b>Correo electrónico</b></label>
-                <input type="text" placeholder="Correo electrónico*" name="email" required>
+                <input type="text" placeholder="Correo electrónico*" name="email" required="">
                 <br>
                 <label for="contraseña"><b>Contraseña</b></label>
-                <input type="password" placeholder="Contraseña*" name="contraseña" required>
+                <input type="password" placeholder="Contraseña*" name="contraseña" required="">
 
                 <input type="submit" name="InicioSesiónf" value="Iniciar sesión" class="IniciarSesión DecoradoBotón">
                 <label>
@@ -74,17 +74,19 @@
                 <hr id="AntesSociales">
                 <script lang="JavaScript" src="JavaScript/Redes Sociales.js"></script>
                 <h3><a href="mailto:dantecc10@gmail.com" class="TítuloArtículo" id="ContactoCorreo">Contacto</a></h3>
-                <img class="RedSocial" alt="Ícono de Facebook" id="Facebook" onclick="javascript:AbrirFacebook();" src="IMG/íconoFacebook.png" />
-                <img class="RedSocial" alt="Ícono de WhatsApp" id="WhatsApp" onclick="javascript:AbrirWhatsApp();" src="IMG/íconoWhatsApp.png" />
-                <img class="RedSocial" alt="Ícono de Messenger" id="Messenger" onclick="javascript:AbrirMessenger();" src="IMG/íconoMessenger.png" />
-                <img class="RedSocial" alt="Ícono de YouTube" id="YouTube" onclick="javascript:AbrirYouTube();" src="IMG/íconoYouTube.png" />
-                <img class="RedSocial" alt="Ícono de Telegram" id="Telegram" onclick="javascript:AbrirTelegram();" src="IMG/íconoTelegram.png" />
-                <img class="RedSocial" alt="Ícono de Twitter" id="Twitter" onclick="javascript:AbrirTwitter();" src="IMG/íconoTwitter.png" />
-                <img class="RedSocial" alt="Ícono de GitHub" id="GitHub" onclick="javascript:AbrirGitHub();" src="IMG/íconoGitHub.png" />
+                <img class="RedSocial" alt="Ícono de Facebook" id="Facebook" onclick="javascript:AbrirFacebook();" src="IMG/íconoFacebook.png">
+                <img class="RedSocial" alt="Ícono de WhatsApp" id="WhatsApp" onclick="javascript:AbrirWhatsApp();" src="IMG/íconoWhatsApp.png">
+                <img class="RedSocial" alt="Ícono de Messenger" id="Messenger" onclick="javascript:AbrirMessenger();" src="IMG/íconoMessenger.png">
+                <img class="RedSocial" alt="Ícono de YouTube" id="YouTube" onclick="javascript:AbrirYouTube();" src="IMG/íconoYouTube.png">
+                <img class="RedSocial" alt="Ícono de Telegram" id="Telegram" onclick="javascript:AbrirTelegram();" src="IMG/íconoTelegram.png">
+                <img class="RedSocial" alt="Ícono de Twitter" id="Twitter" onclick="javascript:AbrirTwitter();" src="IMG/íconoTwitter.png">
+                <img class="RedSocial" alt="Ícono de GitHub" id="GitHub" onclick="javascript:AbrirGitHub();" src="IMG/íconoGitHub.png">
             </div>
             <p id="copyright">Copyright © Dante Castelán Carpinteyro - <b><i>Führer Industries</i></b> - 2022</p>
         </footer>
     </div>
+
+
 </body>
 
 </html>
